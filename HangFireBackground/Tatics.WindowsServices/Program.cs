7bf7557d599d190cf376134a4cc174e9bec9a328
Diogo Martins de Assis <74463348+diogomrtsa@users.noreply.github.com>
2023-12-16 17:59:17 -0300

    Criando projeto Console com outro nome

---
+++ HangFireBackground/Tatics.WindowsServices/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Tatics.WindowsServices;
+
+IHost host = Host.CreateDefaultBuilder()
+    .UseWindowsService(configure =>
+    {
+        configure.ServiceName = "Tactical Windows Services";
+    })
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<CleanupFolderService>();
+    })
+    .Build();
+
+host.Run();
