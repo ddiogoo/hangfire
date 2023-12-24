00d86e5d2e3792b5a75afcbab6619065896e22a7
Diogo Martins de Assis <74463348+diogomrtsa@users.noreply.github.com>
2023-12-24 18:42:47 -0300

    Adicionando serviço do hangfire, definindo rota de acesso ao dashboard do hangfire

--- HangFire.Web/Program.cs
+++ HangFire.Web/Program.cs
@@ -1,5 +1,20 @@
+using Hangfire;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.AddConsole();
+
+builder.Services.AddHangfire(config =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    config.UseSqlServerStorage(connectionString);
+    config.UseColouredConsoleLogProvider();
+});
+builder.Services.AddHangfireServer();
+
 var app = builder.Build();
+
+app.MapHangfireDashboard("/hangfire");
 
 app.MapGet("/", () => "Hello World!");
 
