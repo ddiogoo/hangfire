1c40f0bd3044eec5a1ad3b88481c10c9391a419c
Diogo Martins de Assis <74463348+diogomrtsa@users.noreply.github.com>
2023-12-24 00:04:59 -0300

    Create CleanupTempFolderJob.cs

---
+++ HangFireBackground/WebHostedServiceQuartz/CleanupTempFolderJob.cs
@@ -0,0 +1,52 @@
+﻿using Quartz;
+
+namespace WebHostedServiceQuartz;
+
+public class CleanupTempFolderJob : IJob
+{
+    private ILogger<CleanupTempFolderJob> _logger;
+    private string _tempDirectory;
+
+    public CleanupTempFolderJob(ILogger<CleanupTempFolderJob> logger)
+    {
+        _logger = logger;
+
+        var tempDirectory = Path.GetTempPath();
+        _tempDirectory = Path.Combine(tempDirectory, "CleanUpTempFolderService");
+
+        _logger.LogInformation($"Temp directory is {tempDirectory}");
+    }
+
+    public Task Execute(IJobExecutionContext context)
+    {
+        if (!Directory.Exists(_tempDirectory))
+        {
+            Directory.CreateDirectory(_tempDirectory);
+        }
+        _logger.LogInformation($"Checking folder: {_tempDirectory}");
+
+        var files = Directory
+            .GetFiles(_tempDirectory, "*.*", SearchOption.AllDirectories)
+            .Where(f => File.GetLastWriteTime(f) < DateTime.Now.AddSeconds(-5));
+
+        if (files.Any())
+        {
+            _logger.LogInformation($"Found {files.Count()} files to delete");
+            foreach (var file in files)
+            {
+                try
+                {
+                    File.Delete(file);
+                    _logger.LogInformation($"Deleting file {file}");
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, $"Failed to delete file {file}");
+                }
+            }
+        }
+
+        _logger.LogInformation("Shutgiting down worker");
+        return Task.FromResult(true);
+    }
+}
