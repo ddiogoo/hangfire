3d2685e9dd99afd34e90bc96c56353eddf2eab02
Diogo Martins de Assis <74463348+diogomrtsa@users.noreply.github.com>
2023-12-26 23:01:01 -0300

    Tarefa agendada que converte conteúdo do link em html.

--- HangFire.Web/Program.cs
+++ HangFire.Web/Program.cs
@@ -30,4 +30,32 @@ app.MapGet("/pull", (IBackgroundJobClient client) =>
     client.Enqueue<WebPuller>(p => p.GetRssItemUrlAsync(url, tempPath));
 });
 
+app.MapGet("/sync", (IBackgroundJobClient client) =>
+{
+    var directory = $"c:\\rss";
+    var filename = "consultwithgriff.json";
+
+    var path = Path.Combine(directory, filename);
+    var json = File.ReadAllText(path);
+    var rssItemUrls = JsonSerializer.Deserialize<List<string>>(json);
+
+    var outputPath = Path.Combine(directory, "output");
+    if(!Directory.Exists(outputPath)) Directory.CreateDirectory(outputPath);
+
+    if (rssItemUrls == null || rssItemUrls.Count == 0) return;
+    var delayInSeconds = 5;
+
+    foreach(var url in rssItemUrls)
+    {
+        var u = new Uri(url);
+        var stub = u.Segments.Last();
+        if (stub.EndsWith('/')) stub = stub.Substring(0, stub.Length - 1);
+        stub += ".html";
+
+        var filePath = Path.Combine(outputPath, stub);
+        client.Schedule<WebPuller>(p => p.DownloadFileFromUrl(url, filePath), TimeSpan.FromSeconds(delayInSeconds));
+        delayInSeconds += 5;
+    }
+});
+
 app.Run();
