fde432fa8cc1153c78d1a1618bdf423c49ac00b4
Diogo Martins de Assis <74463348+diogomrtsa@users.noreply.github.com>
2023-12-26 23:00:00 -0300

    Criando método que cria uma lista de links obtidas de um xml específico.

--- HangFire.Web/Jobs/WebPuller.cs
+++ HangFire.Web/Jobs/WebPuller.cs
@@ -13,5 +13,24 @@ namespace HangFire.Web.Jobs
             _logger = logger;
         }
 
+        public async Task GetRssItemUrlAsync(string rssFeedUrl, string filename)
+        {
+            var directory = Path.GetDirectoryName(filename);
+            if(!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+            }
+
+            var client = new HttpClient();
+            var rssContent = await client.GetStringAsync(rssFeedUrl);
+
+            using var xmlReader = XmlReader.Create(new StringReader(rssContent));
+            var feed = SyndicationFeed.Load(xmlReader);
+
+            var rssItemUrls = feed.Items.Select(item => item.Links.FirstOrDefault()?.Uri.AbsoluteUri).ToList();
+            var json = JsonSerializer.Serialize(rssItemUrls);
+            await File.WriteAllTextAsync(filename, json);
+        }
+
     }
 }
