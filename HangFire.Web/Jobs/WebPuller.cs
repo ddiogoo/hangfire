e4a723ab58b6b6a58d726881b15697d2c30ff93f
Diogo Martins de Assis <74463348+diogomrtsa@users.noreply.github.com>
2023-12-26 23:00:13 -0300

    Método que realiza download

--- HangFire.Web/Jobs/WebPuller.cs
+++ HangFire.Web/Jobs/WebPuller.cs
@@ -32,5 +32,27 @@ namespace HangFire.Web.Jobs
             await File.WriteAllTextAsync(filename, json);
         }
 
+        public async Task DownloadFileFromUrl(string url, string filePath)
+        {
+            using var client = new HttpClient();
+            using(_logger.BeginScope($"DownloadFileFromUrl({url}, {filePath})"))
+            {
+                try
+                {
+                    _logger.LogInformation($"Downloading file from {url}...");
+                    using var response = await client.GetAsync(url);
+                    response.EnsureSuccessStatusCode();
+
+                    _logger.LogInformation($"Writing file to {filePath}");
+                    await using var stream = await response.Content.ReadAsStreamAsync();
+                    await using var fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write);
+
+                    await stream.CopyToAsync(fileStream);
+                } catch(HttpRequestException e)
+                {
+                    _logger.LogError(e, $"Error downloading file from {url}");
+                }
+            }
+        }
     }
 }
